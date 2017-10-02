0076dfb92532ef2755cd06a640a1110d683d89ec
rmdcoding <rmdcoding@gmail.com>
2017-10-02 10:56:50 -0700

    Added proof of concept for parallel grep.

---
+++ Shell/parallel-grep.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+:'
+Written by Rigobert M.D.
+
+This script is an example of a parallel bash script, spawning processes.
+Parallel grep beta, proof of cocept. Still need to be able to show colored
+pattern in stdout or output file.
+
+Usage: ./parallel-grep.sh [number of nodes] [pattern] [filename]
+'
+nodes=$1
+end=$((nodes - 1)) 
+filename=$3
+pattern=$2
+
+#This will split the file into sizeable chunks based on nodes
+split -d -n $nodes $filename
+
+#Spawn processes to process their parts of the file
+for i in $(seq 0 $end);
+do
+    #Will show the line number that the patter was found in
+    grep -n $pattern x0$((i)) >> par-grep-output.txt &
+done
+
+wait
+
+#Show the file
+cat par-grep-output.txt
+
+#Remove the files created when done. This causes more latency than needed.
+rm par-grep-output.txt x0*
